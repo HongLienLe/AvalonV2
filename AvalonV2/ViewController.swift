a0ea2f76f684b5984923ab6c1c88ce7bc9bcf72d
Hong Le <Hongle050@gmail.com>
2018-11-09 10:35:45 +0000

    Create an array and takes user input and adds the element to the Array

--- AvalonV2/ViewController.swift
+++ AvalonV2/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var avalonTitle: UILabel!
+    @IBOutlet weak var startingPlayerTitle: UILabel!
+    @IBOutlet weak var nameGenerated: UILabel!
+    @IBOutlet weak var userInputName: UITextField!
+    @IBOutlet weak var addPlayerButton: UIButton!
+    @IBOutlet weak var generatorButton: UIButton!
+    
+    // Create an empty array for userInput
+    var listOfNames = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func addPlayerButtonPressed(_ sender: Any) {
+        // saved user input as a variable
+        let savedName = userInputName.text
+        listOfNames.append(savedName!)
+        
+    }
+    @IBAction func generatorButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
