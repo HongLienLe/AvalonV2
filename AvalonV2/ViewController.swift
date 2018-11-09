f61dc24fdaa5dab1c9e9838ffcc16a393b683746
Hong Le <Hongle050@gmail.com>
2018-11-09 09:31:44 +0000

    Initial Commit

---
+++ AvalonV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvalonV2
+//
+//  Created by Hong Le on 09/11/2018.
+//  Copyright © 2018 Hong Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
